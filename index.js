1f56c46d99c492ca9a0f37dae30fb3782acc1c15
Jordan Ashbacher <jordan.ashbacher@gmail.com>
2021-05-18 14:23:33 -0500

    create birthday app, installs dependencies, creates index.js file

---
+++ index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'redux-saga'
+import { logger } from 'redux-logger'
+
+import rootReducer from './src/reducers/_root.reducer'
+import rootSaga from './src/sagas/_root.saga'
+
+import App from './App'
+
+const sagaMiddleware = createSagaMiddleware()
+
+const middlewareList = process.env.NODE_ENV === 'development' ?
+    [sagaMiddleware, logger] :
+    [sagaMiddleware]
+
+const store = createStore(
+    rootReducer,
+    applyMiddleware(...middlewareList)
+)
+
+sagaMiddleware.run(rootSaga)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('react-root')
+)
